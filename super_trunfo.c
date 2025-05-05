19574ebf11ce7e772de78c4c02351dc3c3c59394
Igor Rosanelli <54559317+igor-rosanelli@users.noreply.github.com>
2025-05-05 15:30:57 -0300

    Add files via upload

---
+++ super_trunfo.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+int main(){
+    //variáveis carta 1
+    char estado1, codigo1[4], cidade1[50];
+    int populacao1, pontos1;
+    float area1, pib1, densidade1, capita1;
+
+    //variáveis carta 2
+    char estado2, codigo2[4], cidade2[50];
+    int populacao2, pontos2;
+    float area2, pib2, densidade2, capita2;
+
+    //entrada dados carta 1
+    printf("Digite os dados da carta 1:\n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigo1);
+
+    printf("Nome da Cidade: ");
+    getchar(); // limpa o buffer antes de usar fgets
+    fgets(cidade1, 50, stdin);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área (em km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib1);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontos1);
+
+    densidade1 = populacao1 / area1;
+    capita1 = (pib1 * 1000000000) / populacao1;
+
+    // Entrada dados carta 2
+    printf("\nDigite os dados da Carta 2:\n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+
+    printf("Código da Carta (ex: B02): ");
+    scanf("%s", codigo2);
+
+    printf("Nome da Cidade: ");
+    getchar(); // limpa o buffer antes de usar fgets
+    fgets(cidade2, 50, stdin);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área (em km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib2);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontos2);
+
+    densidade2 =  populacao2 / area2;
+    capita2 =  (pib2 * 1000000000) / populacao2;
+
+
+    // Exibição dados Carta 1
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s", cidade1); // fgets já inclui \n
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", capita1);
+
+
+     // Exibição dados Carta 2
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s", cidade2); // fgets já inclui \n
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", capita2);
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
